56ea650efe43ddd536093ba5b39c2ba5eb420edc
Daniel Azevedo Guimaraes <danielazevedo2k12@gmail.com>
2017-08-25 13:27:09 -0300

    updt functions

--- src/Laracasts/Flash/functions.php
+++ src/Laracasts/Flash/functions.php
@@ -9,12 +9,13 @@ if ( ! function_exists('flash')) {
      * @param  string      $level
      * @return \Laracasts\Flash\FlashNotifier
      */
-    function flash($message = null, $level = 'info')
+    function flash($message = null, $level = 'info', $title=null)
     {
         $notifier = app('flash');
 
         if ( ! is_null($message)) {
-            return $notifier->message($message, $level);
+            $title = ( strpos( ' error ', $level ) === FALSE ? $title : 'Erro' );
+            return $notifier->message($message, $level, $title);
         }
 
         return $notifier;
